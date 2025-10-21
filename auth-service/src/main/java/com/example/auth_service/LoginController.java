bff249478ea38f2b2a89064cfd1898425da58679
vanquyen <quyenphamvan636@gmail.com>
2025-10-21 22:44:05 +0700

    [Update][QuyenVp] Update auth-service

--- auth-service/src/main/java/com/example/auth_service/LoginController.java
+++ auth-service/src/main/java/com/example/auth_service/LoginController.java
@@ -1,31 +1,49 @@
 package com.example.auth_service;
 
-import org.springframework.security.core.GrantedAuthority;
+import com.nimbusds.jwt.JWT;
+import com.nimbusds.jwt.JWTParser;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.text.ParseException;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
+
 
 @Controller
 public class LoginController {
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
+
     @GetMapping("/login/success")
-    public String loginSuccess(OAuth2AuthenticationToken authentication) {
-        List<String> roles = authentication.getAuthorities()
-                .stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.toList());
+    public String loginSuccess(OAuth2AuthenticationToken authentication) throws ParseException {
+        OAuth2AuthorizedClient client = authorizedClientService
+                .loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(), authentication.getName());
 
+        String accessTokenValue = client.getAccessToken().getTokenValue();
 
-        if (roles.contains("ROLE_ADMIN")) {
-            return "redirect:http://localhost:8082/dashboard/admin"; // admin-portal-service
-        } else if (roles.contains("ROLE_STAFF")) {
-            System.out.println("Toi la staff");
-            return "redirect:http://localhost:8083/dashboard/staff"; // staff-portal-service
-        } else if (roles.contains("ROLE_CUSTOMER")) {
-            return "redirect:http://localhost:8084/dashboard/customer"; // customer-portal-service
+        JWT jwt = JWTParser.parse(accessTokenValue);
+        Map<String, Object> claims = jwt.getJWTClaimsSet().getClaims();
+
+        Map<String, Object> resourceAccess = (Map<String, Object>) claims.get("resource_access");
+        Map<String, Object> clientAccess = (Map<String, Object>) resourceAccess.get("ktx-client");
+        List<String> roles = (List<String>) clientAccess.get("roles");
+
+        System.out.println("Extracted roles: " + roles);
+
+        if (roles.contains("ADMIN")) {
+            return "redirect:http://localhost:8082/dashboard/admin";
+        } else if (roles.contains("STAFF")) {
+            return "redirect:http://localhost:8083/dashboard/staff";
+        } else if (roles.contains("CUSTOMER")) {
+            return "redirect:http://localhost:8084/dashboard/customer";
         }
-        return "redirect:/error";
+
+        return "redirect:/access-denied";
     }
 }
--- auth-service/src/main/java/com/example/auth_service/SecurityConfig.java
+++ auth-service/src/main/java/com/example/auth_service/SecurityConfig.java
@@ -1,10 +1,16 @@
 package com.example.auth_service;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -16,7 +22,23 @@ public class SecurityConfig {
                 )
                 .oauth2Login(oauth2 -> oauth2
                         .defaultSuccessUrl("/login/success", true)
-                );
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("http://localhost:8080/realms/ktx-realm/protocol/openid-connect/logout")
+                )
+                .csrf(AbstractHttpConfigurer::disable); // cần nếu bạn không dùng form login
         return http.build();
     }
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("resource_access.ktx-client.roles");
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return converter;
+    }
+
 }
