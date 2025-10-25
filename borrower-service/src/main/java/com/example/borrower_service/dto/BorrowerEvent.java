7080ec2558b21db92f447bb03fb46dcd70ae8b7e
TuanLuong333 <tuanbk0111@gmail.com>
2025-10-25 13:25:36 +0700

    [Update][TuanLuong]Config kafka for borrower-service and producer-service

---
+++ borrower-service/src/main/java/com/example/borrower_service/dto/BorrowerEvent.java
@@ -0,0 +1,18 @@
+package com.example.borrower_service.dto;
+
+
+import com.example.borrower_service.entity.Borrower;
+import com.example.product_service.entity.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BorrowerEvent {
+    private String message;
+    private Borrower borrower;
+    private Product product;
+    private int quantity;
+}
